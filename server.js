c3d3e8d6fb21062a5366f8f5712334a5135cb7bb
km4891 <kmorarity@gmail.com>
2020-10-22 00:00:00 -0700

    nightly push

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,13 +9,65 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+});
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+/* Delete a candidate
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result, this, this.changes);
+  }); */
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
   });
 
-// connection function
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
