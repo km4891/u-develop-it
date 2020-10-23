33c02897ed77b215800e03d37fc3af505630c6d5
km4891 <kmorarity@gmail.com>
2020-10-22 18:37:40 -0700

    candidates done

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ozzy', 'Osbourne', 1),
+  ('Mac', 'Miller', 1),
+  ('Dimebag', 'Darrell', 0),
+  ('John', 'Lennon', 1),
+  ('Killer', 'Mike', 1),
+  ('Jimi', 'Hendrix', 0),
+  ('Post', 'Malone', 0),
+  ('Eightball', 'MJG', 1),
+  ('Wax', 'Mustang', 1),
+  ('Neil', 'Fallon', 1);
